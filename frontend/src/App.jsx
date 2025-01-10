c58740f7a7bf2075ddfbf009d5700ddb9f4a672d
Param-rai <paramrai0202@gmail.com>
2025-01-10 22:13:08 +0530

    client_basic_folder_struvture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import Project from "./screens/Project";
+import UserAuth from "./auth/UserAuth";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>Hello Duniyaa</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/project" element={<Project />} />
+    </Routes>
+  );
 };
 
 export default App;
